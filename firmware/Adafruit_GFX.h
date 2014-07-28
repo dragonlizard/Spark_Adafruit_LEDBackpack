56a1c6ea53a2f535ea2bd3b4d060234a25f60465
dragonlizard <dragonlizard@users.noreply.github.com>
2014-07-28 23:02:21 +0200

    Update Adafruit_GFX.h

--- firmware/Adafruit_GFX.h
+++ firmware/Adafruit_GFX.h
@@ -1,12 +1,12 @@
 #ifndef _ADAFRUIT_GFX_H
 #define _ADAFRUIT_GFX_H
 
-#if ARDUINO >= 100
- #include "Arduino.h"
+//#if ARDUINO >= 100
+// #include "Arduino.h"
  #include "Print.h"
-#else
- #include "WProgram.h"
-#endif
+//#else
+// #include "WProgram.h"
+//#endif
 
 #define swap(a, b) { int16_t t = a; a = b; b = t; }
 
